228dd00b5ed1fd801ff8de700bd7eba5edb525d3
NodeSpell <150180798+nodespell@users.noreply.github.com>
2023-11-13 23:45:15 +0000

    deploy new files

--- src/App.js
+++ src/App.js
@@ -8,21 +8,21 @@ function Root() {
   <div>
     <Routes>
       <Route
-        element={<>zhiqi.art<div><React.Fragment key=".0"><Link to="/">Home</Link><Link to="/about">About</Link><Link to="/photos">Photos</Link></React.Fragment></div><Outlet /></>}
+        element={<div style={{margin: '10px'}}><React.Fragment key=".0"><div style={{'align-items': 'center', display: 'flex', 'justify-content': 'space-between'}}><React.Fragment key=".0"><div style={{display: 'flex'}}><React.Fragment key=".0"><div style={{color: 'rgb(160, 30, 250)', fontSize: '30px', fontWeight: '200', 'padding-right': '10px'}}><Link to="/">Home</Link></div><div style={{color: 'rgb(160, 30, 250)', fontSize: '30px', fontWeight: '200', 'padding-right': '10px'}}><Link to="/about">About</Link></div><div style={{color: 'rgb(160, 30, 250)', fontSize: '30px', fontWeight: '200', 'padding-right': '10px'}}><Link to="/photos">Photos</Link></div></React.Fragment></div><div style={{color: 'rgb(180, 30, 20)', fontSize: '20px', fontWeight: '100'}}>zhiqi.art</div></React.Fragment></div><Outlet /></React.Fragment></div>}
         path=""
       >
         <React.Fragment key=".0">
           <Route
-            element={<img src="https://nodespell-test-bucket.s3.eu-west-2.amazonaws.com/assets/654c07d77eeae9f388478454/cloroq16q000j3b6igg6nk0jx" />}
-            path="/photos"
+            element="🚀"
+            path="/"
           />
           <Route
             element="Build great things."
             path="/about"
           />
           <Route
-            element="🚀"
-            path="/"
+            element={<img src="https://nodespell-test-bucket.s3.eu-west-2.amazonaws.com/assets/654c07d77eeae9f388478454/cloroq16q000j3b6igg6nk0jx" />}
+            path="/photos"
           />
         </React.Fragment>
       </Route>
